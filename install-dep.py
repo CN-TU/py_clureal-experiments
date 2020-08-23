cb5d680c6f3fbcff15ae161dafb67d6cebb4290a
cn <cn@buster.cn.tuwien.ac.at>
2020-08-23 18:15:40 +0200

    First commit

---
+++ install-dep.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+import dependencies
+dependencies.assert_pkgs({
+    'numpy': 'numpy',
+    'pandas': 'pandas',
+    'scipy': 'scipy',
+    'sklearn': 'scikit-learn',
+})
+
